2d13c3170d17e4edf2358849eded91a44739d01c
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-06-03 22:50:57 +0900

    [level 0] Title: 모음 제거, Time: 12.46 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(int i=0; i<my_string.length(); i++){
+            if(my_string.charAt(i)=='a' || my_string.charAt(i)=='e' || my_string.charAt(i)=='i' || my_string.charAt(i)=='o' || my_string.charAt(i)=='u'){
+                continue;
+            }else{
+                answer += my_string.charAt(i);
+            }
+        }
+        return answer;
+    }
+}
