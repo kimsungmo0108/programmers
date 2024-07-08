18ca380fc4b8e42b5d408b22607e658f7034eada
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-07-08 20:11:20 +0900

    [level 0] Title: l로 만들기, Time: 604.01 ms, Memory: 376 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for(int i=0; i<myString.length(); i++){
+            if(myString.charAt(i) < 'l'){
+                answer += "l";
+            }else{
+                answer += String.valueOf(myString.charAt(i));
+            }
+        }
+        return answer;
+    }
+}
