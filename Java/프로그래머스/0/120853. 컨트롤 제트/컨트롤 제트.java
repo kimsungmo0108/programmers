c949b875adbda6667c1723c1d1aef5ae76ec0e64
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-10-27 16:37:11 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.24 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] arr = s.split(" ");
+        for(int i=0; i<arr.length; i++){
+            if(arr[i].equals("Z")){
+                answer -= Integer.valueOf(arr[i-1]);
+            }else{
+                answer += Integer.valueOf(arr[i]);    
+            }    
+        }
+        return answer;
+    }
+}
